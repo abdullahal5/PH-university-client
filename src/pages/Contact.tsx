7a4911bf9e29594e5142420fc6885b58ed676afe
abdullahal5 <abdullahalfahin183@gmail.com>
2024-07-02 23:07:10 -0700

    project layout setup, file structure and redux setup

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,9 @@
+const Contact = () => {
+  return (
+    <div>
+      this contact page
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/routes/routes.tsx
@@ -0,0 +1,48 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
+import Register from "../pages/Register";
+import { adminRoutes } from "./admin.routes";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+    ],
+  },
+  {
+    path: "/admin",
+    element: <App />,
+    children: adminRoutes,
+  },
+  {
+    path: "/faculty",
+    element: <App />,
+    children: adminRoutes,
+  },
+  {
+    path: "/student",
+    element: <App />,
+    children: adminRoutes,
+  },
+  {
+    path: "login",
+    element: <Contact />,
+  },
+  {
+    path: "register",
+    element: <Register />,
+  },
+]);
+
+export default router;
